055b423b606dbb0d1547818dec3539db4b193f9a
mflorent <mflorent@student.42.fr>
2017-11-07 21:23:15 +0100

    ft_memcopy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,8 +1,14 @@
 #include <string.h>
 
-void *ft_memcpy(char *dst, const char src, size_t n)
+void *ft_memcpy(char *dst, const char *src, size_t n)
 {
+	size_t c = 0;
 
+	while (c < n)
+	{
+		dst[c] = src[c];
+		c++;
+	}
 
 	return (dst);
 }
