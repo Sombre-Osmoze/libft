a04343175ebf0e7f8f57d9fd11b9e13032bcd6fb
mflorent <mflorent@student.42.fr>
2017-11-14 00:16:43 +0100

    Implémentation de "ft_memccpy"
    Mettro
    
    TOdod finir fonction

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -2,8 +2,19 @@
 
 void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
+	size_t i;
+	unsigned char *src_p;
+	unsigned char *dst_p;
 
+	i = 0;
+	src_p = src;
+	dst_p = dst;
+	while (i < n && )
+	{
+		dst_p[i] = src_p[i]
 
+		i++;
+	}
 
 
 
