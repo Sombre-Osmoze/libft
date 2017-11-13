ea8e748c8927698aac9efb4200c02d5b57523888
mflorent <mflorent@student.42.fr>
2017-11-13 13:05:41 +0100

    Rajout du fichier header "libft.h"

---
+++ libft.h
@@ -0,0 +1,18 @@
+//
+//  libft.h
+//  libft
+//
+//  Created by Marcus Florentin on 13/11/2017.
+//  Copyright © 2017 Marcus Florentin. All rights reserved.
+//
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <string.h>
+
+void	*ft_memset(char *b, int c, size_t len);
+void	ft_bzero(char *s, size_t n);
+void	*ft_memcpy(char *dst, const char *src, size_t n);
+
+#endif /* libft_h */
