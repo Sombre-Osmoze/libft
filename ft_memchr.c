8998173ae794772a219bdf0aa9fece852f63a171
mflorent <mflorent@student.42.fr>
2017-11-14 23:26:39 +0100

    Implémentation de la fonction "ft_memmove"
    Création du fichier "ft_memchr"
    
    TODO : Terminer memmove >>>>>

---
+++ ft_memchr.c
@@ -0,0 +1,9 @@
+//
+//  ft_memchr.c
+//  libft
+//
+//  Created by Marcus Florentin on 14/11/2017.
+//  Copyright © 2017 Marcus Florentin. All rights reserved.
+//
+
+#include <stdio.h>
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,9 +3,18 @@
 void *ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t i;
+	unsigned char *src_p;
+	unsigned char *dst_p;
 
 	i = 0;
+	src_p = (unsigned char *)src;
+	dst_p = dst;
+	while (i < len) {
+		
 
+
+
+	}
 
 
 	return (dst);
