77bf31dcdfed120396c46bffcacd57c66a0839d3
mflorent <mflorent@student.42.fr>
2017-11-14 20:44:07 +0100

    Mise à la norme

--- libft.h
+++ libft.h
@@ -1,22 +1,26 @@
-//
-//  libft.h
-//  libft
-//
-//  Created by Marcus Florentin on 13/11/2017.
-//  Copyright © 2017 Marcus Florentin. All rights reserved.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 20:32:47 by marcusflo         #+#    #+#             */
+/*   Updated: 2017/11/14 20:36:09 by marcusflo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <string.h>
+# include <string.h>
 
-#define NBFT  5
+# define NBFT  5
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(char *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void *ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
-#endif /* libft_h */
+#endif
