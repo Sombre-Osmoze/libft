d3cae8b379c880cd5cdec514c0747a3713b17a3e
mflorent <mflorent@student.42.fr>
2017-11-16 23:55:02 +0100

    Done : "ft_strnstr"

---
+++ ft_strnstr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/16 23:45:47 by marcusflo         #+#    #+#             */
+/*   Updated: 2017/11/16 23:45:51 by marcusflo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*buff;
+	size_t	i;
+
+	buff = NULL;
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (*haystack && buff == NULL && (len > 0))
+	{
+		if (*haystack == *needle)
+		{
+			buff = (char *)haystack;
+			i = 0;
+			while (needle[i] != '\0' && buff != NULL)
+			{
+				if (*buff != needle[i])
+					buff = NULL;
+				i++;
+				buff++;
+			}
+		}
+		haystack++;
+		len--;
+	}
+	return (buff);
+}
--- libft.h
+++ libft.h
@@ -34,5 +34,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_stchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
