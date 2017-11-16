8faf6580eb671cc0f3765f53a557a21a6d5f43ff
mflorent <mflorent@student.42.fr>
2017-11-16 18:37:04 +0100

    Done : "ft_strncat"

--- libft.h
+++ libft.h
@@ -15,7 +15,7 @@
 
 # include <string.h>
 
-# define NBFT  11
+# define NBFT  14
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(char *s, size_t n);
@@ -27,5 +27,8 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *s1, char *s2);
+char	*ft_strncat(char *s1, const char *s2, size_t n);
 
 #endif
