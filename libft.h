e6220d879d01d4b254376a91c70c9cb1da26f738
mflorent <mflorent@student.42.fr>
2017-11-16 12:32:46 +0100

    Création de ft_strdup

--- libft.h
+++ libft.h
@@ -15,7 +15,7 @@
 
 # include <string.h>
 
-# define NBFT  8
+# define NBFT  9
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(char *s, size_t n);
@@ -25,5 +25,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 
 #endif
