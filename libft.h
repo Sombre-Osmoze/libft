b4a01f1dc9bd8174cd37da46b4addb8af593f9b4
mflorent <mflorent@student.42.fr>
2017-11-17 15:26:43 +0100

    Done : "ft_isalpha"

--- libft.h
+++ libft.h
@@ -38,5 +38,6 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(char *str);
+int	ft_isalpha(int c);
 
 #endif
