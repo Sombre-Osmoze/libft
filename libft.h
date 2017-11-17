97e01d92c16f5196e25a335b75bda3d6f1463732
mflorent <mflorent@student.42.fr>
2017-11-17 15:20:01 +0100

    Done : "ft_atoi"

--- libft.h
+++ libft.h
@@ -37,5 +37,6 @@ char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_atoi(char *str);
 
 #endif
