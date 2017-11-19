38214c335508785f7c01ae13dc9b0ba3204a68c9
mflorent <mflorent@student.42.fr>
2017-11-19 22:18:49 +0100

    ;

--- libft.h
+++ libft.h
@@ -51,6 +51,6 @@ void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
 void	ft_strdel(char **as);
 void	ft_strclr(char *s);
-void	ft_striter(char *s, void (*f)(char *))
+void	ft_striter(char *s, void (*f)(char *));
 
 #endif
