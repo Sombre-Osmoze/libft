a279e2a7d4130254419b43983f7d3d8e53245813
mflorent <mflorent@student.42.fr>
2017-11-22 22:59:05 +0100

    Eric le con

---
+++ ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 20:28:50 by marcusflo         #+#    #+#             */
+/*   Updated: 2017/11/22 20:28:52 by marcusflo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list	*p_curr;
+
+	p_curr = *alst;
+	while (p_curr->next != NULL)
+	{
+		(del)(p_curr->content, p_curr->content_size);
+		free(p_curr);
+		p_curr = p_curr->next;
+	}
+	(*alst) = NULL;
+}
---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/22 19:33:38 by marcusflo         #+#    #+#             */
+/*   Updated: 2017/11/22 19:33:39 by marcusflo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new_mail;
+
+	new_mail = NULL;
+	new_mail = malloc(sizeof(t_list));
+	if (new_mail != NULL)
+	{
+		new_mail->next = NULL;
+		if (content != NULL)
+		{
+			new_mail->content = malloc(sizeof(content_size));
+			if (new_mail->content == NULL)
+				new_mail->content_size = 0;
+			else
+				new_mail->content_size = 0;
+		}
+		else
+			new_mail->content_size = 0;
+	}
+	return (new_mail);
+}
