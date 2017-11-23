f01c2fb66d3afdf03b4ce844a5146c13898132e9
mflorent <mflorent@student.42.fr>
2017-11-23 17:49:13 +0100

    Erreur

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,13 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((*s1 || *s2) && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
-		if (*s1 != *s2)
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-		s1++;
-		s2++;
-		n++;
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
 	}
 	return (0);
 }
