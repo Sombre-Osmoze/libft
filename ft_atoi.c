872a9eed7efe9042958c089f3122e393870b80f0
Marcus Florentin <mflorent@student.42.fr>
2017-11-23 17:20:17 +0100

    Atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: marcusflorentin <marvin@42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 15:16:15 by marcusflo         #+#    #+#             */
-/*   Updated: 2017/11/17 15:16:19 by marcusflo        ###   ########.fr       */
+/*   Updated: 2017/11/23 17:19:24 by mflorent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,20 @@ int	ft_atoi(char *str)
 	nb = 0;
 	sign = 1;
 	while (str[i] == '\0' || str[i] == '\t' || str[i] == '\r'
-			|| str[i] == '\0' || str[i] == '\f')
+			|| str[i] == ' ' || str[i] == '\f' || str[i] == '\v')
 		i++;
 	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			sign = -1;
-		i++;
-	}
 	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
 		nb = nb * 10;
-		nb = nb + (str[i] - 48);
-		i++;
+		nb = nb + (str[i++] - 48);
+		if (nb > 9223372036854775807)
+			if (sign < 0)
+				return (0);
+			else if (sign > 0)
+				return (-1);
 	}
 	return (nb * sign);
 }
