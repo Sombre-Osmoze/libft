216f0dcdf623d03bcec2bd3f1c227e7f02119995
mflorent <mflorent@student.42.fr>
2017-11-27 12:22:49 +0100

    Correction prototype

--- libft.h
+++ libft.h
@@ -23,7 +23,7 @@ typedef struct	s_list
 }				t_list;
 
 void			*ft_memset(void *b, int c, size_t len);
-void			ft_bzero(char *s, size_t n);
+void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
