e708909dc15d5ca7a21785c41663daf454292aab
mflorent <mflorent@student.42.fr>
2017-11-27 13:34:13 +0100

    Déplacement du correctif d'erreur

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -29,9 +29,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 			i[1] = 0;
 			while (needle[i[1]] != '\0' && buff != NULL)
 			{
-				if ((buff[i[1]] != needle[i[1]]) || (i[0] + i[1] < len))
+				if (buff[i[1]] != needle[i[1]])
 					buff = NULL;
-				else
+				else if (((i[0] + 1) + i[1]) < len)
 					i[1]++;
 			}
 		}
