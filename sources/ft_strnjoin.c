89e80a08828aa14ea2f9c4296a19f15e81b48784
Marcus Florentin <fmarcus28@gmx.fr>
2017-12-26 20:06:36 +0100

    Correction "ft_strnjoin"

--- sources/ft_strnjoin.c
+++ sources/ft_strnjoin.c
@@ -19,16 +19,16 @@ char	*ft_strnjoin(const char *s1, size_t n1, const char *s2, size_t n2)
 	char	*new;
 	char	*buff;
 
-	if ((new = ft_strnew((n1 + n2 - 2) * sizeof(char) + 1)) == NULL)
+	if ((new = ft_strnew((int)(n1 + n2) * sizeof(char) + 1)) == NULL)
 		return (NULL);
 	ft_bzero(i, sizeof(i));
 	buff = new;
-	while (*s1 && i[0] < n1)
+	while (s1 && *s1 && i[0] < n1)
 	{
 		*new++ = *s1++;
 		i[0] += 1;
 	}
-	while (*s2 && i[1] < n2)
+	while (s2 && *s2 && i[1] < n2)
 	{
 		*new++ = *s2++;
 		i[1] += 1;
