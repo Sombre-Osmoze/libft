5d31eff529c91c46ccc1c5bf44a9a5c4d24ce236
Marcus Florentin <fmarcus28@gmx.fr>
2018-05-29 05:47:34 +0200

    Mise à jour et correction de bugs
    Ajout de fonction de tests

--- sources/ft_putstr_fd.c
+++ sources/ft_putstr_fd.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <libft.h>
+#include "libft.h"
 
 void	ft_putstr_fd(const char *s, int fd)
 {
